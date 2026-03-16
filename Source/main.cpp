1fe5d38f9ff13e2620c07ee6c884a13977196762
Crisel <justus.walter-helk@web.de>
2026-03-16 19:25:30 +0100

    Small test

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
 
+#include "CMemory.h"
+#include "Debug.h"
+
 int main() {
-	std::cout << "Hello, World!" << std::endl;
+	Memory* memory = new Memory();
+
+	MemoryPrinter printer(*memory);
+	printer.StartSession();
+
+	memory->Write16Bit(4096, 0);
+
+	printer.WriteDeltaMemory();
+
+	memory->Write8Bit(300, 2);
+
+	printer.WriteDeltaMemory();
+
+	printer.EndSession();
+
+	delete memory;
 }
